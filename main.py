66c40767ff740e71f788fb5e8fafc600bcb8cfb3
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2023-01-03 15:00:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
-def start():
-def main():
-    start()
+#===================================================
+# Підключенні бібліотеки.
+#===================================================
+import openpyxl
+import os
+#===================================================
 
-if __name__=='__main__':
+
+#===================================================
+# Головні змінні.
+#===================================================
+file_way=os.path.dirname(__file__)
+file_name=os.path.basename(__file__)
+file_name=file_name.split('.')
+file_name=file_name[0]
+database_excel=file_name+'.xlsx'
+#===================================================
+
+
+#===================================================
+# Функції
+#===================================================
+def database_excel_open():
+    workbook = openpyxl.load_workbook(database_excel)
+    workbook_sheet=workbook['sheet1']
+    a1=workbook_sheet['A1'].value
+    a2=workbook_sheet['A2'].value
+    a3=workbook_sheet['A3'].value
+    print(a1)
+
+
+def database_excel_create():
+    workbook = openpyxl.Workbook()
+    workbook_sheet=workbook.active
+    workbook_sheet.title = 'sheet1'
+    workbook_sheet['A1'] = 1
+    workbook_sheet['A2'] = 2
+    workbook_sheet['A3'] = 3
+    workbook.save(database_excel)
+
+
+def fail_message():
+    print('Виявлена помилка.')
+    print('Всі налаштування скинуто до початкових.')
+    input()
+
+#===================================================
+
+
+#===================================================
+# Точка входу.
+#===================================================
+def main():
+    try:
+        database_excel_open()
+    except:
+        database_excel_create()
+        fail_message()
+
+
+if __name__ == '__main__':
     main()
+#===================================================
